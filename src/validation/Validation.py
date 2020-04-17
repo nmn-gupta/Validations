08a448e3ea31063aa5a967aab1948384000d0559
naman <naman.gupta_cs18@gla.ac.in>
2020-04-17 15:43:18 +0530

    create a method check_Password()

--- src/validation/Validation.py
+++ src/validation/Validation.py
@@ -2,3 +2,7 @@ print("Press 1. for validate password")
 print("Press 2. for validate Email")
 print("Press 3. for validate Identifier name")
 print("Press 4. for validate IPv4 address")
+
+
+def check_Password():
+    input_password = input("Enter the password: ")
